47fb105f2d92a85d1ca931e5729d00d62ce80170
nicka-CC <128600081+nicka-CC@users.noreply.github.com>
2025-01-13 08:22:16 +0300

    feat(server): add events

--- server/router/Events/EventsRouter.js
+++ server/router/Events/EventsRouter.js
@@ -3,20 +3,9 @@ import { PrismaClient } from "@prisma/client";
 import { authCheckMiddleware } from "../middleware/middleWareRouter.js";
 import multer from "multer";
 import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
-
-// Получаем путь к текущему файлу
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const uploadDir = path.join(__dirname, "/../../uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir);
+    cb(null,'./uploads');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
